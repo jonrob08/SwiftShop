e488ee16cdc6db3994f55fd49fcfa57a367135bb
jonrob <jonrob08@gmail.com>
2022-12-14 12:49:50 -0700

    chore: Adding content to footer

--- swiftshop/src/components/Footer.js
+++ swiftshop/src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import { BsLinkedin, BsGithub, BsYoutube, BsInstagram } from 'react-icons/bs'
 
 const Footer = () => {
   return (
@@ -34,15 +35,47 @@ const Footer = () => {
           <div className="row">
             <div className="col-4">
               <h4 className='mb-4 text-white'>Contact Us</h4>
-              <div></div>
+              <div>
+                <address className='text-white fs-6'>
+                  635 S. Ellis St., <br />
+                  Chandler, Arizona <br />
+                  85224-4953
+                </address>
+                <a href="tel:+1 7175929969" className="mt-4 d-block mb-2 text-white">+1 (717) 592-9969</a>
+                <a href="mailto:jonrob08@gmail.com" className="mt-4 d-block mb-2 text-white">jonrob08@gmail.com</a>
+                <div className="social_icons d-flex align-items-center gap-30">
+                  <a className='text-white' href="">
+                    <BsGithub className='fs-4' />
+                  </a>
+                  <a className='text-white' href="">
+                    <BsLinkedin className='fs-4' />
+                  </a>
+                  <a className='text-white' href="">
+                    <BsYoutube className='fs-4' />
+                  </a>
+                  <a className='text-white' href="">
+                    <BsInstagram className='fs-4' />
+                  </a>
+                </div>
+              </div>
             </div>
             <div className="col-3">
               <h4 className='mb-4 text-white'>Information</h4>
-              <div></div>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='mb-1 py-2 text-white'>Privacy Policy</Link>
+                <Link className='mb-1 py-2 text-white'>Refund Policy</Link>
+                <Link className='mb-1 py-2 text-white'>Shipping Policy</Link>
+                <Link className='mb-1 py-2 text-white'>Terms & Conditions</Link>
+                <Link className='mb-1 py-2 text-white'>Blog</Link>
+              </div>
             </div>
             <div className="col-3">
               <h4 className='mb-4 text-white'>Account</h4>
-              <div></div>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='mb-1 py-2 text-white'>About Us</Link>
+                <Link className='mb-1 py-2 text-white'>FAQ</Link>
+                <Link className='mb-1 py-2 text-white'>Contact</Link>
+              </div>
             </div>
             <div className="col-2">
               <h4 className='mb-4 text-white'>Quick Links</h4>
