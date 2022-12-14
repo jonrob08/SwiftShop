d0d037bd15fa3daf011dd38864c6d19751a18e78
jonrob <jonrob08@gmail.com>
2022-12-13 23:35:00 -0700

    chore: Add initial route setup

--- swiftshop/src/components/Layout.js
+++ swiftshop/src/components/Layout.js
@@ -1,8 +1,17 @@
 import React from 'react'
+// Utilizing an Outlet because it allows nested UI from a parent route to show up when child routes are rendered. If the parent route matches exactly, it will render a child index route or nothing at all 
+// https://reactrouter.com/en/main/components/outlet
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <>
+        <Header />
+        <Outlet />
+        <Footer />
+    </>
   )
 }
 
