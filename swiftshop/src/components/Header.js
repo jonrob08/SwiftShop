9eaf60208803cd223ac469980596749831838c04
jonrob <jonrob08@gmail.com>
2022-12-14 00:32:01 -0700

    top header started

--- swiftshop/src/components/Header.js
+++ swiftshop/src/components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+     <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className='text-white mb-0'>Free Shipping Over $100 & Free Returns</p>
+            </div>
+            <div className="col-6">
+              <p className='text-end text-white mb-0'>
+                Call Us: <a className='text-white' href="tel:+1 7175929969">(717) 592 - 9969</a>
+              </p>
+            </div>
+          </div>
+        </div>
+     </header>
+    </>
   )
 }
 
