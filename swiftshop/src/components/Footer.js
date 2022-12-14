357f58f548dc9a1038661cb2e62852ae6afa49d6
jonrob <jonrob08@gmail.com>
2022-12-14 12:32:47 -0700

    chore: General footer layout hammered out

--- swiftshop/src/components/Footer.js
+++ swiftshop/src/components/Footer.js
@@ -1,8 +1,73 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer className='py-4'>
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-5">
+              <div className="footer-top-data d-flex gap-30 align-items-center">
+                <img src="images/newsletter.png" alt="newsletter" />
+                <h2 className='mb-0 text-white'>Sign Up for our Newsletter</h2>
+              </div>
+            </div>
+            <div className="col-7">
+            <div class="input-group">
+              <input 
+                type="text" 
+                className="form-control py-1" 
+                placeholder="Your email address" 
+                aria-describedby="basic-addon2" 
+              />
+              <span class="input-group-text p-2" id="basic-addon2">
+               Subscribe
+              </span>
+            </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className='py-4'>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-4">
+              <h4 className='mb-4 text-white'>Contact Us</h4>
+              <div></div>
+            </div>
+            <div className="col-3">
+              <h4 className='mb-4 text-white'>Information</h4>
+              <div></div>
+            </div>
+            <div className="col-3">
+              <h4 className='mb-4 text-white'>Account</h4>
+              <div></div>
+            </div>
+            <div className="col-2">
+              <h4 className='mb-4 text-white'>Quick Links</h4>
+              <div className='footer-links d-flex flex-column'>
+                <Link className='mb-1 py-2 text-white'>Laptops</Link>
+                <Link className='mb-1 py-2 text-white'>Headphones</Link>
+                <Link className='mb-1 py-2 text-white'>Tablets</Link>
+                <Link className='mb-1 py-2 text-white'>Watches</Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+      <footer className='py-4'>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy; { new Date().getFullYear() }, SwiftShop Powered by JonRob Technologies
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
