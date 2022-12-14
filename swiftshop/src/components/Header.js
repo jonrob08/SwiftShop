74c10a786fca6b73068b9e652a438bdfd949ab98
jonrob <jonrob08@gmail.com>
2022-12-14 10:42:12 -0700

    chore: Added links and images to header
    Also started working on site styling in CSS file, these links will also be dynamically added at some point, but for now I'm just focusing on UI/UX

--- swiftshop/src/components/Header.js
+++ swiftshop/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import { BsSearch } from 'react-icons/bs'
 
 const Header = () => {
   return (
@@ -17,6 +18,67 @@ const Header = () => {
             </div>
           </div>
         </div>
+     </header>
+     <header className="header-upper py-3">
+      <div className="container-xxl">
+        <div className="row align-items-center">
+          <div className="col-2">
+            <h2>
+              <Link className='text-white'>Dev Corner</Link>
+            </h2>
+          </div>
+          <div className="col-5">
+            <div class="input-group">
+              <input 
+                type="text" 
+                className="form-control py-2" 
+                placeholder="Search for your perfect product" 
+                aria-describedby="basic-addon2" 
+              />
+              <span class="input-group-addon p-3" id="basic-addon2">
+                <BsSearch className='fs-5'/>
+              </span>
+            </div>
+          </div>
+          <div className="col-5">
+            <div className="header-upper-links d-flex align-items-center justify-content-between">
+              <div>
+                <Link className='d-flex align-items-center gap-10'>
+                  <img src="images/compare.svg" alt="compare" />
+                  <p>
+                    Compare <br /> Products
+                  </p>
+                </Link>
+              </div>
+              <div>
+                <Link className='d-flex align-items-center gap-10'>
+                  <img src="images/wishlist.svg" alt="wishlist" />
+                  <p>
+                  Favorites <br /> Wishlist
+                  </p>
+                </Link>
+              </div>
+              <div>
+                <Link className='d-flex align-items-center gap-10'>
+                  <img src="images/user.svg" alt="login" />
+                  <p>
+                  Login To <br /> My Account
+                  </p>
+                </Link>
+              </div>
+              <div>
+                <Link className='d-flex align-items-center gap-10'>
+                  <img src="images/cart.svg" alt="cart" />
+                  <div className="d-flex flex-column">
+                    <span className='badge bg-white text-dark'>0</span>
+                    <p>$500</p>
+                  </div>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
      </header>
     </>
   )
