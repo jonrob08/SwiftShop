1e7da232b063924eeaa0cb12b4aafc284c43b0ff
jonrob <jonrob08@gmail.com>
2022-12-14 17:15:57 -0700

    setting up home page banner content

--- swiftshop/src/pages/Home.js
+++ swiftshop/src/pages/Home.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner p-3 position-relative p-3">
+                <img src="images/main-banner-2.jpeg" className='img-fluid rounded-3' alt="main banner" />
+                <div className="main-banner-content position-absolute">
+                  <h4>Long-Lasting Battery Life.</h4>
+                  <h4>Noise Cancellation.</h4>
+                  <h5>BOSE Alpha</h5>
+                  <p>From $999.99 or $74.33/mo.</p>
+                  <Link>BUY NOW</Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
