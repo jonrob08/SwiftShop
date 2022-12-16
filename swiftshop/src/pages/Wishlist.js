1b5861e41e96214bb88697f9f066aec8f31a4d0b
jonrob <jonrob08@gmail.com>
2022-12-16 16:57:27 -0700

    creating wishlist card, working on position and style

--- swiftshop/src/pages/Wishlist.js
+++ swiftshop/src/pages/Wishlist.js
@@ -8,6 +8,20 @@ const Wishlist = () => {
     <>
       <Meta title={title} />
       <BreadCrumb title={title} /> 
+      <div className="wishlist-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="wishlist-card">
+              <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                <div className="wishlist-card-image">
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
