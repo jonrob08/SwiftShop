d3f33f3fc9f36cfda36f9ed07c33524fbf1d2a7d
jonrob <jonrob08@gmail.com>
2022-12-16 14:14:41 -0700

    feat: completed grid logic for product cards

--- swiftshop/src/components/ProductCard.js
+++ swiftshop/src/components/ProductCard.js
@@ -8,7 +8,7 @@ const ProductCard = (props) => {
 
   return (
     <>
-        <div className={` ${ location.pathname === "/store" ? `col-${ grid }` : "col-3"} `}>
+        <div className={` ${ location.pathname === "/store" ? `gr-${ grid }` : "col-3"} `}>
         <Link className="product-card position-relative">
             <div className="wishlist-icon position-absolute">
                 <Link><img src="images/wish.svg" alt="wishlist" /></Link>
@@ -40,7 +40,7 @@ const ProductCard = (props) => {
             </div>
         </Link>
     </div>
-    <div className={` ${ location.pathname === "/store" ? `col-${ grid }` : "col-3"} `}>
+    <div className={` ${ location.pathname === "/store" ? `gr-${ grid }` : "col-3"} `}>
         <Link className="product-card position-relative">
             <div className="wishlist-icon position-absolute">
                 <Link><img src="images/wish.svg" alt="wishlist" /></Link>
