14bc19a1b2792d339e42cacd24e7fb7b68d1e3bf
jonrob <jonrob08@gmail.com>
2022-12-16 16:51:03 -0700

    chore: finish comparison page UI

--- swiftshop/src/pages/Compare.js
+++ swiftshop/src/pages/Compare.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import Color from '../components/Color';
 
 const Compare = () => {
     const title = "Compare Products"
@@ -19,7 +20,7 @@ const Compare = () => {
                         </div>
                         <div className="compare-product-details">
                             <h5 className="title">New watch Best Timekeeper 8GB</h5>
-                            <h6 className="price mb-3">$299</h6>
+                            <h6 className="price mb-3 mt-3">$299</h6>
                         
                             <div>
                             <div className="product-detail">
@@ -40,7 +41,50 @@ const Compare = () => {
                             </div>
                             <div className="product-detail">
                                 <h5>Color</h5>
+                                <Color />
+                            </div>
+                            <div className="product-detail">
+                                <h5>Size</h5>
+                                <div className="d-flex gap-10">
+                                    <p>S</p>
+                                    <p>M</p>
+                                    <p>L</p>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-3">
+                    <div className="compare-product-card position-relative">
+                    <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                        <div className="product-card-image">
+                            <img src="images/watch.jpg" alt="watch" />
+                        </div>
+                        <div className="compare-product-details">
+                            <h5 className="title">New watch Best Timekeeper 8GB</h5>
+                            <h6 className="price mb-3 mt-3">$299</h6>
+                        
+                            <div>
+                            <div className="product-detail">
+                                <h5>Brand:</h5>
+                                <p>Watch Brand</p>
+                            </div>
+                            <div className="product-detail">
+                                <h5>Type</h5>
+                                <p>Watch Type</p>
+                            </div>
+                            <div className="product-detail">
+                                <h5>SKU</h5>
+                                <p>13524</p>
+                            </div>
+                            <div className="product-detail">
+                                <h5>Availability</h5>
                                 <p>In Stock</p>
+                            </div>
+                            <div className="product-detail">
+                                <h5>Color</h5>
+                                <Color />
                             </div>
                             <div className="product-detail">
                                 <h5>Size</h5>
