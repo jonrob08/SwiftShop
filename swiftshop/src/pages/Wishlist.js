a83b473049eeb7ef34bbd3b00442d21272da729e
jonrob <jonrob08@gmail.com>
2022-12-16 17:07:37 -0700

    chore: complete wishlist UI

--- swiftshop/src/pages/Wishlist.js
+++ swiftshop/src/pages/Wishlist.js
@@ -12,10 +12,38 @@ const Wishlist = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-3">
-              <div className="wishlist-card">
+              <div className="wishlist-card position-relative">
               <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
                 <div className="wishlist-card-image">
-
+                  <img src="images/watch.jpg" className='img-fluid w-100' alt="watch" />
+                </div>
+                <div className='py-3 px-3'>
+                  <h5 className='title'>Watch watch title title here blahhh blahhhh</h5>
+                  <h6 className='price'>$999</h6>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="wishlist-card position-relative">
+              <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                <div className="wishlist-card-image">
+                  <img src="images/watch.jpg" className='img-fluid w-100' alt="watch" />
+                </div>
+                <div className='py-3 px-3'>
+                  <h5 className='title'>Watch watch title title here blahhh blahhhh</h5>
+                  <h6 className='price'>$999</h6>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="wishlist-card position-relative">
+              <img src="images/cross.svg" alt="cross" className="position-absolute cross img-fluid" />
+                <div className="wishlist-card-image">
+                  <img src="images/watch.jpg" className='img-fluid w-100' alt="watch" />
+                </div>
+                <div className='py-3 px-3'>
+                  <h5 className='title'>Watch watch title title here blahhh blahhhh</h5>
+                  <h6 className='price'>$999</h6>
                 </div>
               </div>
             </div>
