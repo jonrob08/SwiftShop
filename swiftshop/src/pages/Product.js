df32f70e6f0b32d38823546b3baa7887412f75d6
jonrob <jonrob08@gmail.com>
2022-12-17 13:03:22 -0700

    chore: add functionality to determine if product has been bought

--- swiftshop/src/pages/Product.js
+++ swiftshop/src/pages/Product.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactStars from "react-rating-stars-component";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ProductCard from '../components/ProductCard'
 
 const Product = () => {
   const title = "Dynamic Product Name";
+  const [productBought, setProductBought] = useState(false)
+
   return (
     <>
       <Meta title={title} />
@@ -20,14 +23,42 @@ const Product = () => {
       <section className="description-wrapper py-5">
         <div className="container-xxl">
             <div className="row">
-                <div className="col-12"></div>
+                <div className="col-12">
+                    <div className="bg-white p-3">
+                        <h4>Description</h4>
+                        <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis, neque sit amet lacinia suscipit, odio nisl vulputate nisi, eget eleifend libero metus ac lacus. Maecenas tempus eget lacus id porta. Proin a elit augue. Etiam ipsum est, tempus quis velit id, vehicula molestie felis. Nulla placerat arcu quis dolor rutrum ultrices. 
+                        </p>
+                    </div>
+                </div>
             </div>
         </div>
       </section>
       <section className="reviews-wrapper py-5">
         <div className="container-xxl">
             <div className="row">
-                <div className="col-12"></div>
+                <div className="col-12">
+                    <div className="review-head d-flex justify-content-between align-items-end">
+                        <div>
+                            <h4 className="mb-2">Customer Reviews</h4>
+                            <div>
+                                <ReactStars 
+                                    count={5} 
+                                    size={24} 
+                                    value="3" 
+                                    edit={false} 
+                                    activeColor='#ffd700'
+                                />
+                            </div>
+                            <p className="mb-0">Based on 2 reviews</p>
+                        </div>
+                        {productBought && (
+                            <div>
+                                <a href="">Write a Review</a>
+                            </div>
+                        )}
+                    </div>
+                </div>
             </div>
         </div>
       </section>
