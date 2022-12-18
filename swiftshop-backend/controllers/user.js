98107cfe5599f9779f70a32b96635b5f6f2a6fe5
jonrob <jonrob08@gmail.com>
2022-12-18 13:57:59 -0700

    chore: set up user model and auth routes

---
+++ swiftshop-backend/controllers/user.js
@@ -0,0 +1,17 @@
+const User = require('../models/user')
+
+const createUser = async (req, res) => {
+    const email = req.body.email
+    const findUser = await User.findOne(email)
+    if (!findUser) {
+        const newUser = User.create(req.body)
+        res.json(newUser)
+    } else {
+        res.json({
+            msg: "This user already exists",
+            success: false,
+        })
+    }
+}
+
+module.exports={createUser}
--- swiftshop-backend/index.js
+++ swiftshop-backend/index.js
@@ -4,10 +4,16 @@ const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 3002
 dbConnect();
-
+const authRouter = require('./routes/auth')
 app.use('/', (req, res) => {
     console.log('Hello from the server!')
 })
+
+app.use('/api/user', authRouter)
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`You are now listening to the smooth sounds of PORT ${PORT}`)
