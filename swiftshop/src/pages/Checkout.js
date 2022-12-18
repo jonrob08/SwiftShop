af54fbcda77d4c2d6aaca9e445a7e477fbb31b4d
jonrob <jonrob08@gmail.com>
2022-12-17 19:26:11 -0700

    chore: finished right side info

--- swiftshop/src/pages/Checkout.js
+++ swiftshop/src/pages/Checkout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {BiArrowBack} from 'react-icons/bi'
+import { BiArrowBack } from 'react-icons/bi'
+import { Watch } from '../images'
 
 const Checkout = () => {
 
@@ -31,12 +32,13 @@ const Checkout = () => {
                                     </li>
                                 </ol>
                             </nav>
-                            <h4 className="title">
+                            <h4 className="title total">
                                 Contact Information
                             </h4>
                             <p className="user-details">
                                 Jonathan Robson (jonrob08@gmail.com)
                             </p>
+                            <h4 className='mb-3'>Shipping Address</h4>
                             <form action="" className='d-flex gap-15 flex-wrap justify-content-between'>
                                 <div className='w-100'>
                                     <select name="" className='form-control form-select' id="">
@@ -75,7 +77,39 @@ const Checkout = () => {
                             </form>
                         </div>
                     </div>
-                    <div className="col-5"></div>
+                    <div className="col-5">
+                        <div className='border-btm py-4'>
+                            <div className="d-flex gap-10 mb-2 align-items-center">
+                                <div className='w-75 d-flex gap-10'>
+                                    <div className='w-25 position-relative'>
+                                    <span style={{ top: "-10px", right: "2px" }} className="badge bg-secondary text-white rounded-circle p-3 position-absolute">1</span>
+                                        <img className='img-fluid' src={Watch} alt="" />
+                                    </div>
+                                    <div>
+                                        <h5 className="total-price">Product Title</h5>
+                                        <p className='"total-price"'>SKU #: 19387589</p>
+                                    </div>
+                                </div>
+                                <div className='flex-grow-1'>
+                                    <h5 className='total'>$100</h5>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='border-btm py-4'>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <p className='total'>Subtotal</p>
+                                <p className='total-price'>$ 10,000</p>
+                            </div>
+                            <div className='d-flex justify-content-between align-items-center'>
+                                <p className='mb-0 total'>Shipping</p>
+                                <p className='mb-0 total-price'>$ 10,000</p>
+                            </div>
+                        </div>
+                        <div className='d-flex justify-content-between align-items-center border-btm py-4'>
+                            <h4 className='total'>Total</h4>
+                            <h5 className='total-price'>$ 10,000</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
