98e2b361beb3efd772822c6f6246357f97433e2c
jonrob <jonrob08@gmail.com>
2022-12-18 13:47:46 -0700

    chore: connect to mongodb atlas

--- swiftshop-backend/index.js
+++ swiftshop-backend/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const dbConnect = require('./config/dbConnect')
 const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 3002
+dbConnect();
+
+app.use('/', (req, res) => {
+    console.log('Hello from the server!')
+})
 
 app.listen(PORT, () => {
     console.log(`You are now listening to the smooth sounds of PORT ${PORT}`)
