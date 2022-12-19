c73c3cbea6b6a5ac94ce30d71c4fd295b42fbfda
jonrob <jonrob08@gmail.com>
2022-12-19 15:11:26 -0700

    bug: fixed middleware bug

--- swiftshop-backend/routes/product.js
+++ swiftshop-backend/routes/product.js
@@ -9,10 +9,10 @@ const {
 const { isAdmin, authMiddleware } = require('../middleware/auth')
 const router = express.Router();
 
-router.post("/", isAdmin, authMiddleware, createProduct);
+router.post("/", authMiddleware, isAdmin, createProduct);
 router.get("/:id", getProduct);
-router.put("/:id", isAdmin, authMiddleware, updateProduct);
-router.delete("/:id", isAdmin, authMiddleware, deleteProduct);
+router.put("/:id", authMiddleware, isAdmin, updateProduct);
+router.delete("/:id", authMiddleware,  isAdmin, deleteProduct);
 router.get("/", getAllProducts);
 
 module.exports = router;
