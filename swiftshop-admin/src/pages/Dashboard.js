345f78f9817ba03864acc8a4965c36617ab5aadf
jonrob <jonrob08@gmail.com>
2022-12-20 16:12:44 -0700

    chore: working on Enquiries page

--- swiftshop-admin/src/pages/Dashboard.js
+++ swiftshop-admin/src/pages/Dashboard.js
@@ -1,8 +1,162 @@
 import React from 'react'
+import { BsArrowDownRight, BsArrowUpRight } from 'react-icons/bs'
+import { Column } from '@ant-design/plots';
+import { Table } from 'antd';
+const columns = [
+  {
+    title: 'Serial#',
+    dataIndex: 'key',
+  },
+  {
+    title: 'Name',
+    dataIndex: 'name',
+  },
+  {
+    title: 'Product',
+    dataIndex: 'product',
+  },
+  {
+    title: 'Status',
+    dataIndex: 'status',
+  },
+];
+const data1 = [];
+for (let i = 0; i < 46; i++) {
+  data1.push({
+    key: i,
+    name: `Edward King ${i}`,
+    age: 32,
+    address: `London, Park Lane no. ${i}`,
+  });
+}
 
 const Dashboard = () => {
+    const data = [
+      {
+        type: 'Jan',
+        sales: 38,
+      },
+      {
+        type: 'Feb',
+        sales: 52,
+      },
+      {
+        type: 'Mar',
+        sales: 61,
+      },
+      {
+        type: 'Apr',
+        sales: 145,
+      },
+      {
+        type: 'May',
+        sales: 48,
+      },
+      {
+        type: 'June',
+        sales: 38,
+      },
+      {
+        type: 'July',
+        sales: 38,
+      },
+      {
+        type: 'Aug',
+        sales: 38,
+      },      {
+        type: 'Sept',
+        sales: 38,
+      },      {
+        type: 'Oct',
+        sales: 38,
+      },      {
+        type: 'Nov',
+        sales: 38,
+      },      {
+        type: 'Dec',
+        sales: 38,
+      },
+    ];
+    const config = {
+      data,
+      xField: 'type',
+      yField: 'sales',
+      color: ({ type }) => {
+  
+        return "#ffd333";
+      },
+      label: {
+        position: 'middle',
+        style: {
+          fill: '#0',
+          opacity: 0.6,
+        },
+      },
+      xAxis: {
+        label: {
+          autoHide: true,
+          autoRotate: false,
+        },
+      },
+      meta: {
+        type: {
+          alias: 'Month',
+        },
+        sales: {
+          alias: 'Income',
+        },
+      },
+    };
+  
   return (
-    <div>Dashboard</div>
+    <div>
+      <h3 className='mb-4'>Dashboard</h3>
+      <div className='d-flex justify-content-between align-items-center gap-3'>
+        <div className='d-flex justify-content-between align-items-end flex-grow-1 bg-white p-3 rounded-3'>
+          <div>
+            <p className=''>Total</p>
+            <h4 className='mb-0'>$16053.93</h4>
+          </div>
+            <div className='d-flex flex-column align-items-end'>
+              <h6 className='red'><BsArrowDownRight /> 32%</h6>
+              <p className='mb-0'>Compare To April 2022</p>
+            </div>
+        </div>
+        <div className='d-flex justify-content-between align-items-end  flex-grow-1 bg-white p-3 rounded-3'>
+          <div>
+            <p className=''>Total</p>
+            <h4 className='mb-0'>$16053.93</h4>
+          </div>
+            <div className='d-flex flex-column align-items-end'>
+              <h6 className='green'><BsArrowUpRight /> 32%</h6>
+              <p className='mb-0'>Compare To April 2022</p>
+            </div>
+        </div>
+        <div className='d-flex justify-content-between align-items-end  flex-grow-1 bg-white p-3 rounded-3'>
+          <div>
+            <p className=''>Total</p>
+            <h4 className='mb-0'>$16053.93</h4>
+          </div>
+            <div className='d-flex flex-column align-items-end'>
+              <h6 className='green'><BsArrowUpRight /> 32%</h6>
+              <p className='mb-0'>Compare To April 2022</p>
+            </div>
+        </div>
+      </div>
+       <div className="mt-4">
+        <h3 className='mb-4'>Income Statistics</h3>
+        <div>
+        <Column {...config} />;
+        </div>
+       </div>
+       <div className="mt-4">
+        <h3 className='mb-4'>Recent Orders</h3>
+        <div>
+      <Table columns={columns} dataSource={data1} />
+        </div>
+        <h3 className="mb-4">Recent Reviews</h3>
+       </div>
+    </div>
   )
 }
 
---
+++ swiftshop-admin/src/pages/Enquiries.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Enquiries = () => {
+  return (
+    <div>
+        <h3 className="mb-4">Enquiries</h3>
+    </div>
+  )
+}
+
+export default Enquiries
